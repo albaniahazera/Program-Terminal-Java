f1daf41c3043039a1f5a098fbbc94b54a9a6abfd
albania hazera <116944586+albaniahazera@users.noreply.github.com>
2025-02-06 17:20:19 +0700

    update.1.0

--- ProgramUtama.java
+++ ProgramUtama.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class ProgramUtama {
     private static Scanner scanner = new Scanner(System.in);
@@ -7,11 +8,14 @@ public class ProgramUtama {
         pilihanUtama();
     }
     public static void pilihanUtama() {
-        System.out.println("\t\t\nMain Menu\n");
+        System.out.println("==========================================================");
+        System.out.println("|Main Menu|\n");
         System.out.println("1.Calculation");
         System.out.println("2.Coming soon");
         System.out.println("0.Exit");
-        System.out.print("\nChoose: ");
+        System.out.println("==========================================================");
+        System.out.println("\nChoose a number !");
+        System.out.print("===> ");
         if (scanner.hasNextInt()) {
             int inputUser = scanner.nextInt();
             scanner.nextLine();
@@ -22,100 +26,175 @@ public class ProgramUtama {
                 System.exit(0);
             }else {
                 menghapusOutput();
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 System.out.println("\t\tInvalid Input !");
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 pilihanUtama();
                 menghapusOutput();
             }
         }else {
             scanner.nextLine();
             menghapusOutput();
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             System.out.println("\t\tInvalid Input !");
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             pilihanUtama();
             menghapusOutput();
         }
     }
     public static void menuPenghitung() {
-        System.out.println("\n|Calculation|\n");
+        System.out.println("==========================================================");
+        System.out.println("|Calculation|\n");
         System.out.println("1.Increase");
         System.out.println("2.Subtraction");
         System.out.println("3.Division");
+        System.out.println("4.Multiplication");
+        System.out.println("5.Multiplication table");
         System.out.println("0.Back to main menu");
-        System.out.print("\nChoose: ");
+        System.out.println("==========================================================");
+        System.out.println("\nChoose a number !");
+        System.out.print("===> ");
         if (scanner.hasNextInt()) {
             int inputUser = scanner.nextInt();
             scanner.nextLine();
             if (inputUser == 1) {
+                menghapusOutput();
                 pertambahan();
             }else if (inputUser == 2) {
+                menghapusOutput();
                 pengurangan();
             }else if (inputUser == 3) {
+                menghapusOutput();
                 pembagian();
+            }else if (inputUser == 4) {
+                menghapusOutput();
+                perkalian();
+            }else if (inputUser == 5) {
+                menghapusOutput();
+                tabelPerkalian();
             }else if (inputUser == 0) {
                 menghapusOutput();
                 pilihanUtama();
             }else {
                 menghapusOutput();
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 System.out.println("\t\tInvalid Input !");
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 menuPenghitung();
                 menghapusOutput();
             }
         }else {
             scanner.nextLine();
             menghapusOutput();
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             System.out.println("\t\tInvalid Input !");
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             menuPenghitung();
             menghapusOutput();
         }
     }
     public static void pertambahan() {
-        menghapusOutput();
-        System.out.println("-----------------------------------------------------");
-        System.out.println("\t\t/Increase/\n");
-        System.out.print("Enter a number: ");
-        int A = scanner.nextInt();
-        System.out.print("Enter a number: ");
-        int B = scanner.nextInt();
-        int hasil = A + B ;
-        System.out.println("Result: " + hasil);
-        System.out.println("-----------------------------------------------------\n");
-        pilihankedua();
-        menghapusOutput();
+        try {
+            System.out.println("-----------------------------------------------------");
+            System.out.println("\t\t/Increase/\n");
+            System.out.print("Enter a number: ");
+            double A = scanner.nextInt();
+            System.out.print("Enter a number: ");
+            double B = scanner.nextInt();
+            double hasil = 0;
+            hasil = A + B ;
+            System.out.println("Result: " + hasil);
+            System.out.println("-----------------------------------------------------\n");
+            pilihankedua();
+            menghapusOutput();
+        }catch (InputMismatchException e) {
+            System.out.println("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println("\t\tInvalid Input !");
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            menuPenghitung();
+        }
     }
     public static void pengurangan() {
-        menghapusOutput();
-        System.out.println("-----------------------------------------------------");
-        System.out.println("\t\t/Subtraction/\n");
-        System.out.print("Enter a number: ");
-        int A = scanner.nextInt();
-        System.out.print("Enter a number: ");
-        int B = scanner.nextInt();
-        int hasil = A - B ;
-        System.out.println("Result: " + hasil);
-        System.out.println("-----------------------------------------------------\n");
-        pilihankedua();
-        menghapusOutput();
+        try {
+            System.out.println("-----------------------------------------------------");
+            System.out.println("\t\t/Subtraction/\n");
+            System.out.print("Enter a number: ");
+            double A = scanner.nextInt();
+            System.out.print("Enter a number: ");
+            double B = scanner.nextInt();
+            double hasil = 0;
+            hasil = A - B ;
+            System.out.println("Result: " + hasil);
+            System.out.println("-----------------------------------------------------\n");
+            pilihankedua();
+            menghapusOutput();
+        }catch (InputMismatchException e) {
+            System.out.println("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println("\t\tInvalid Input !");
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            menuPenghitung();
+        }
     }
     public static void pembagian() {
-        menghapusOutput();
-        System.out.println("-----------------------------------------------------");
-        System.out.println("\t\t/Division/\n");
-        System.out.print("Enter a number: ");
-        int A = scanner.nextInt();
-        System.out.print("Enter a number: ");
-        int B = scanner.nextInt();
-        int hasil = A / B ;
-        System.out.println("Result: " + hasil);
-        System.out.println("-----------------------------------------------------\n");
-        pilihankedua();
-        menghapusOutput();
+        try {
+            System.out.println("-----------------------------------------------------");
+            System.out.println("\t\t/Division/\n");
+            System.out.print("Enter a number: ");
+            double A = scanner.nextInt();
+            System.out.print("Enter a number: ");
+            double B = scanner.nextInt();
+            double hasil = 0;
+            hasil = A / B ;
+            System.out.println("Result: " + hasil);
+            System.out.println("-----------------------------------------------------\n");
+            pilihankedua();
+            menghapusOutput();
+        }catch (InputMismatchException e) {
+            System.out.println("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println("\t\tInvalid Input !");
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            menuPenghitung();
+        }
+    }
+    public static void perkalian() {
+        try {
+            System.out.println("-----------------------------------------------------");
+            System.out.println("\t\t/Multiplication/\n");
+            System.out.print("Enter a number: ");
+            double A = scanner.nextInt();
+            System.out.print("Enter a number: ");
+            double B = scanner.nextInt();
+            double hasil = 0;
+            hasil = A * B ;
+            System.out.println("Result: " + hasil);
+            System.out.println("-----------------------------------------------------\n");
+            pilihankedua();
+            menghapusOutput();
+        }catch (InputMismatchException e) {
+            System.out.println("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println("\t\tInvalid Input !");
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            menuPenghitung();
+        }
+    }
+    public static void tabelPerkalian() {
+        try {
+            System.out.println("-----------------------------------------------------");
+            System.out.println("\t\t/Multiplication table/\n");
+            System.out.print("Enter a number: ");
+            int num = scanner.nextInt();
+            for (int i = 1; i <= 12; i++) {
+                System.out.println(num + " x " + i + " = " + (num * i));
+            }
+            System.out.println("-----------------------------------------------------\n");
+            pilihankedua();
+            menghapusOutput();
+        }catch (InputMismatchException e) {
+            System.out.println("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println("\t\tInvalid Input !");
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            menuPenghitung();
+        }
     }
     public static void pilihankedua() {
         System.out.println("\n-----------------------------------------------------");
@@ -123,35 +202,47 @@ public class ProgramUtama {
         System.out.println("\n1.Enter to Increase");
         System.out.println("2.Enter to Subtraction");
         System.out.println("3.Enter to Division");
+        System.out.println("4.Enter to Multiplication");
+        System.out.println("5.Enter to Multiplication Table");
         System.out.println("0.Back to main menu\n");
         System.out.println("-----------------------------------------------------");
-        System.out.print("\nChoose: ");
+        System.out.println("\nChoose a number !");
+        System.out.print("===> ");
         if (scanner.hasNextInt()) {
             int inputUser = scanner.nextInt();
             scanner.nextLine();
             if (inputUser == 1) {
+                menghapusOutput();
                 pertambahan();
             }else if (inputUser == 2) {
+                menghapusOutput();
                 pengurangan();
             }else if (inputUser == 3) {
+                menghapusOutput();
                 pembagian();
+            }else if (inputUser == 4) {
+                menghapusOutput();
+                perkalian();
+            }else if (inputUser == 5) {
+                menghapusOutput();
+                tabelPerkalian();
             }else if (inputUser == 0) {
                 menghapusOutput();
                 menuPenghitung();
             }else {
                 menghapusOutput();
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 System.out.println("\t\tInvalid Input !");
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 pilihankedua();
                 menghapusOutput();
             }
         }else {
             scanner.nextLine();
             menghapusOutput();
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             System.out.println("\t\tInvalid Input !");
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             pilihankedua();
             menghapusOutput();
         }
